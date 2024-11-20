5701f1e57fa7d951e80b32d5e76ccfdbdbda140d
Tehran Bayramov <nicosspyros@gmail.com>
2024-11-21 02:12:24 +0400

    social added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import Tooltip from "./components/Social";
 
 const App = () => {
   return (
@@ -14,11 +15,12 @@ const App = () => {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
+        {/* <Feedbacks /> */}
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
+        <Tooltip />
       </div>
     </BrowserRouter>
   );
