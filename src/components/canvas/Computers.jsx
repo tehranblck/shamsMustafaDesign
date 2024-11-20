ebc21d827fab17e4447c98ea47fb0a0801f6a094
Tehran Bayramov <nicosspyros@gmail.com>
2024-11-21 03:43:19 +0400

    dfsdfd

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -22,7 +22,7 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         scale={isMobile ? 30 : 50}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -6.5]}
+        position={isMobile ? [0, 0, 0] : [0, -3.25, -6.5]}
         rotation={[-0, 1, -0]}
       />
     </mesh>
