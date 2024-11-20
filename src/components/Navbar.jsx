f7b16ef40ca564e4fde6946002fdd012c4cff112
Tehran Bayramov <nicosspyros@gmail.com>
2024-11-21 03:34:20 +0400

    finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <img src={logo} alt='logo' className='w-9 h-9 rounded-full object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Shams &nbsp;
             <span className='sm:block hidden'> |Game Artist </span>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -80,11 +80,7 @@ const Works = () => {
           variants={fadeIn("", "", 0.1, 1)}
           className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'
         >
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
+          The following projects highlight my skills and experience as a game artist through real-world examples of my work. Each project is briefly described, showcasing visual designs, creative processes, and gameplay elements I contributed to. These projects demonstrate my ability to craft immersive worlds, work with various art styles and technologies, and manage end-to-end art production effectively.
         </motion.p>
       </div>
 
